ed2f70b39aeb0c2ce0e7fcdd9b0659eca2b4f52f
Nic Schittenhelm <ebrofi@gmail.com>
2021-04-19 17:03:36 +0200

    Steckbrief Uodate

--- lib/screens/kurs_screen.dart
+++ lib/screens/kurs_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:kurs_2021_app/screens/asmir_hodzic_screen.dart';
 import 'package:kurs_2021_app/screens/hueseyin_s_screen.dart';
 import 'package:kurs_2021_app/screens/jan_torsten_m_screen.dart';
+import 'package:kurs_2021_app/screens/nic_s_screen.dart';
 import 'package:kurs_2021_app/screens/till_h_screen.dart';
 import 'package:kurs_2021_app/screens/unbekannt_screen.dart';
 
@@ -21,6 +22,7 @@ class KursScreen extends StatelessWidget {
           zeileKursteilnehmer("Hüseyin", context, HueseyinSScreen()),
           zeileKursteilnehmer("Till Henkel", context, TillHScreen()),
           zeileKursteilnehmer("Asmir Hodzic", context, AsmirHScreen()),
+          zeileKursteilnehmer("Nic Schittenhelm", context, NicSScreen()),
         ],
       ),
     );
--- lib/screens/nic_s_screen.dart
+++ lib/screens/nic_s_screen.dart
@@ -5,37 +5,179 @@ class NicSScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Nic Schittenhelm"),
-      ),
       body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 100.0),
+            child: Text(
+              "Portfolio",
+              style: GoogleFonts.poppins(
+                  textStyle: TextStyle(
+                fontSize: 24,
+                color: Color.fromRGBO(3,62,140,1),
+                fontWeight: FontWeight.w600,
+                letterSpacing: 1.2,
+              )),
+            ),
+          ),
           SizedBox(
             height: 20,
           ),
-          Image.asset("assets/images/pb.jpg", width: 64),
-          Text("Name: Nic Schittenhelm",
-              style: GoogleFonts.poppins(
-                  textStyle: TextStyle(
-                fontSize: 16,
-                color: Colors.teal,
-              ))),
-          Text("Alter: 21",
-              style: GoogleFonts.poppins(
-                  textStyle: TextStyle(
-                fontSize: 16,
-              ))),
-          Text("Studiengang: Digitale Medien",
-              style: GoogleFonts.poppins(
-                  textStyle: TextStyle(
-                fontSize: 16,
-              ))),
-          Text("Fachsemester: 6",
-              style: GoogleFonts.poppins(
-                  textStyle: TextStyle(
-                fontSize: 16,
-              ))),
+          CircleAvatar(
+            radius: 70,
+            backgroundImage: AssetImage("assets/images/stockimage.jpg"),
+
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Text(
+            "Nic Schittenhelm",
+            style: GoogleFonts.poppins(
+                textStyle: TextStyle(
+              fontSize: 18,
+              color: Color.fromRGBO(3,62,140,1),
+              fontWeight: FontWeight.w500,
+              letterSpacing: .5,
+            )),
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left:70.0),
+            child: Column(
+
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  children: [
+                    Icon(
+                      Icons.school_outlined,
+                      color: Colors.grey[400],
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      "Digitale Medien Student",
+                      style: GoogleFonts.poppins(
+                          textStyle: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w400,
+                        letterSpacing: .5,
+                      )),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 5,),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.access_time_rounded,
+                      color: Colors.grey[400],
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      "Fachsemester 6",
+                      style: GoogleFonts.poppins(
+                          textStyle: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w400,
+                        letterSpacing: .5,
+                      )),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 5,),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.location_on_outlined,
+                      color: Colors.grey[400],
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      "Hochschule Fulda",
+                      style: GoogleFonts.poppins(
+                          textStyle: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w400,
+                        letterSpacing: .5,
+                      )),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 5,),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.account_circle_outlined,
+                      color: Colors.grey[400],
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      "Freiberufler",
+                      style: GoogleFonts.poppins(
+                          textStyle: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w400,
+                        letterSpacing: .5,
+                      )),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 5,),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.local_post_office_outlined,
+                      color: Colors.grey[400],
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      "nic.schittenhelm@hs-fulda.de",
+                      style: GoogleFonts.poppins(
+                          textStyle: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w400,
+                        letterSpacing: .2,
+                      )),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 5,),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.phone_android_outlined,
+                      color: Colors.grey[400],
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      "+49 0170 987654321",
+                      style: GoogleFonts.poppins(
+                          textStyle: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w400,
+                        letterSpacing: .2,
+                      )),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );

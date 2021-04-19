83192155bc1d6b79261766203b00e3665605c5c0
Hüse <hueseyin_sevim@gmx.de>
2021-04-19 14:31:11 +0200

    refactor code

--- lib/screens/hueseyin_s_screen.dart
+++ lib/screens/hueseyin_s_screen.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 class hueseyin_s_screen extends StatelessWidget {
-@override
-Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      title: Text("Hüseyin Sevim"),
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hüseyin Sevim"),
       ),
     );
   }

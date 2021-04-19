30aacc2e07812c0a5a20f224949c5db77b40827f
drmilde <milde@hs-fulda.de>
2021-04-19 13:03:40 +0200

    Liste und Navigation eingebaut

---
+++ lib/screens/jan_torsten_m_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class JanTorstenMScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Jan-Torstens Seite"),
+      ),
+      body: Container(
+        child: Text("Jan-Torsten"),
+      ),
+    );
+
+  }
+}
---
+++ lib/screens/kurs_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:kurs_2021_app/screens/jan_torsten_m_screen.dart';
+
+class KursScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Kursteilnehmer"),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          zeileKursteilnehmer("Prof Dr. habil. Jan-Torsten Milde", context,
+              JanTorstenMScreen()),
+          zeileKursteilnehmer("Sven", context, JanTorstenMScreen()),
+          zeileKursteilnehmer("Kerstin", context, JanTorstenMScreen()),
+        ],
+      ),
+    );
+  }
+
+  Widget zeileKursteilnehmer(String text, BuildContext context, Widget screen) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: GestureDetector(
+        onTap: () {
+          print("ouch");
+          Navigator.of(context)
+              .push(MaterialPageRoute(builder: (context) => screen));
+        },
+        child: Container(
+          //width: 200,
+          color: Colors.green,
+          child: Text(
+            text,
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}

0ec2354157d8e831c0b3350879546059f5319e63
jacksonpanzo <jaccksonpanzo.jp@gmail.com>
2021-04-19 15:37:54 +0200

    mit Bild und Infos

--- lib/screens/jackson_screen.dart
+++ lib/screens/jackson_screen.dart
@@ -1,8 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class JacksonScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Jackson Panzo"),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Image.asset("assets/images/jpanzo.jpg"),
+          Text(
+            "Name: Panzo",
+            style: TextStyle(
+              fontSize: 18,
+            ),
+          ),
+          Text(
+            "Vorname: Jackson",
+            style: TextStyle(
+              fontSize: 18,
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Text("AI-Student",
+            style: GoogleFonts.originalSurfer(
+              textStyle: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }

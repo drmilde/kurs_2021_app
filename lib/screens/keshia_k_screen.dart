104404fbb844d31599661c544e57eef269d9eda5
keku7 <keshia-chiara.kullmann@informatik.hs-fulda.de>
2021-04-21 17:07:20 +0200

    erweitert

--- lib/screens/keshia_k_screen.dart
+++ lib/screens/keshia_k_screen.dart
@@ -1,24 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class KeshiaKScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text("Ich"),
+        title: Icon(Entypo.github),
+        backgroundColor: Colors.lightGreen,
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Image.asset("assets/images/hi.png"),
-          Container(
-            child: Text("Ich bin's",
-              style: TextStyle(
-                fontSize: 20,
-                color: Colors.lightGreen,
-                fontWeight: FontWeight.bold,
-              ),
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            "Ich bin's",
+            style: TextStyle(
+              fontSize: 21,
+              color: Colors.lightGreen,
+              fontWeight: FontWeight.bold,
             ),
           ),
+          SizedBox(
+            height: 20,
+          ),
+          Text("Keshia",
+              style: GoogleFonts.sacramento(
+                  textStyle: TextStyle(
+                fontSize: 40,
+              ))),
         ],
       ),
     );

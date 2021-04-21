7e611b0a47cb2015f5b34f14fd57ba42e056d3ed
Kepp <jasonstolze@yahoo.de>
2021-04-21 12:33:58 +0200

    jason s screen angelegt

---
+++ lib/screens/jason_s_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class JasonSScreen extends StatelessWidget {
+  @override
+  /*Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBar(
+        title: Text("Jason Stolzes krasse Seite"),
+      ),  // AppBar
+      body: Container(
+        child: Text("Hallo ich bin der Jesen"),
+      ),
+    );*/
+  }
+}

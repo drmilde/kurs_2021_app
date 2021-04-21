c45e470e6f6ea0f09d17e32b8c5d9823a057d8c7
Android-Tom <sebastian.heil@informatik.hs-fulda.de>
2021-04-21 12:42:07 +0200

    import

--- lib/screens/kurs_screen.dart
+++ lib/screens/kurs_screen.dart
@@ -5,8 +5,10 @@ import 'package:kurs_2021_app/screens/jan_torsten_m_screen.dart';
 import 'package:kurs_2021_app/screens/michael_h_screen.dart';
 import 'package:kurs_2021_app/screens/nic_s_screen.dart';
 import 'package:kurs_2021_app/screens/pascal_w_screen.dart';
+import 'package:kurs_2021_app/screens/sebastian_h_screen.dart';
 import 'package:kurs_2021_app/screens/till_h_screen.dart';
 import 'package:kurs_2021_app/screens/unbekannt_screen.dart';
+
 
 class KursScreen extends StatelessWidget {
   @override

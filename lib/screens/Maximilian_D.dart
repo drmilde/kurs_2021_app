ed7267a2aed2a898fa5400530a73e14b8e29e2d3
TMDhsfulda <tobias-maximilian.desch@informatik.hs-fulda.de>
2021-04-21 17:30:04 +0200

    Maximilian_D überarbeitet

--- lib/screens/Maximilian_D.dart
+++ lib/screens/Maximilian_D.dart
@@ -1,12 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class MaximilianDScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-      title: Text("Maximilian Desch"),
-    ));
+          title: Text("Maximilian Desch"),
+        ),
+        body: Column(
+          children: [
+            Center(
+              child: Image.asset(
+                "assets/images/knautschi.jpg",
+              ),
+            ),
+            Text("Knautschi ist cool"),
+            Text("Name: Maximilian Desch",
+            style: GoogleFonts.pacifico(
+              textStyle: TextStyle(
+                fontSize: 20
+              )
+            )
+            ),
+            Icon(AntDesign.pluscircleo),
+          ],
+        ));
   }
 }

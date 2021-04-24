baa49dd03b2114a1b4d0d7c09ab307913fa0059a
domenicYR <domenic.rosenbaum54@gmail.com>
2021-04-24 09:06:12 +0200

    domenic_r_screen

--- lib/screens/kurs_screen.dart
+++ lib/screens/kurs_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kurs_2021_app/screens/domenic_r_screen.dart';
 import 'package:kurs_2021_app/screens/maximilian_d_screen.dart';
 import 'package:kurs_2021_app/screens/asmir_hodzic_screen.dart';
 import 'package:kurs_2021_app/screens/hueseyin_s_screen.dart';
@@ -48,6 +49,7 @@ class KursScreen extends StatelessWidget {
             zeileKursteilnehmer("Sven Nowak", context, SvenNScreen()),
             zeileKursteilnehmer("Keshia Kullmann", context, KeshiaKScreen()),
             zeileKursteilnehmer("Katharina Schubert", context, KatharinaSScreen()),
+            zeileKursteilnehmer("Domenic Rosenbaum", context, DomenicRScreen()),
           ]
         ),
       ),

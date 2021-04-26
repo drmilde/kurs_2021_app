b9c0b481b595bf170664266b8bd751b415f20971
[Pascal] <[passi200999@gmail.com]>
2021-04-26 12:27:02 +0200

    Screen Pascal Anpassung BoxDecoration

--- lib/screens/pascal_w_screen.dart
+++ lib/screens/pascal_w_screen.dart
@@ -10,13 +10,35 @@ class PascalWScreen extends StatelessWidget {
       body: SingleChildScrollView(
         child: Center (
           child: Padding(
-            padding: const EdgeInsets.all(15.0),
+            padding: const EdgeInsets.all(5),
            child: Column(
               children: [
                Text("Guuuuude",
                   style: TextStyle(
                     fontSize: 50,
                     backgroundColor: Colors.black38,
+                  ),
+                ),
+                Container(
+
+                  height: 300,
+                  width: 300,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage (
+                        'assets/images/cat.jpg',
+                      ),
+                      fit: BoxFit.fill,
+                    ),
+                    border: Border.all(color: Colors.red[500], width: 5),
+                    borderRadius: BorderRadius.all(Radius.circular(5)),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black,
+                        blurRadius: 10,
+                        offset: Offset(10, 10),
+                      ),
+                    ],
                   ),
                 ),
               ]

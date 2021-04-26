a76100101b068d26f41e8a7b60f8283d50e03dfe
Mateg96 <75450231+Mateg96@users.noreply.github.com>
2021-04-26 11:56:22 +0200

    mein steckbrief

--- lib/screens/kurs_screen.dart
+++ lib/screens/kurs_screen.dart
@@ -1,25 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:kurs_2021_app/screens/carolin_s_screen.dart';
-import 'package:kurs_2021_app/screens/domenic_r_screen.dart';
-import 'package:kurs_2021_app/screens/maximilian_d_screen.dart';
 import 'package:kurs_2021_app/screens/asmir_hodzic_screen.dart';
 import 'package:kurs_2021_app/screens/hueseyin_s_screen.dart';
 import 'package:kurs_2021_app/screens/jan_torsten_m_screen.dart';
 import 'package:kurs_2021_app/screens/jason_s_screen.dart';
-import 'package:kurs_2021_app/screens/katharina_s_screen.dart';
 import 'package:kurs_2021_app/screens/kerstin_g_screen.dart';
-import 'package:kurs_2021_app/screens/keshia_k_screen.dart';
 import 'package:kurs_2021_app/screens/maximilian_r_screen.dart';
-import 'package:kurs_2021_app/screens/merle_l_screen.dart';
 import 'package:kurs_2021_app/screens/michael_h_screen.dart';
 import 'package:kurs_2021_app/screens/nic_s_screen.dart';
 import 'package:kurs_2021_app/screens/pascal_w_screen.dart';
 import 'package:kurs_2021_app/screens/sebastian_h_screen.dart';
-import 'package:kurs_2021_app/screens/sven_n_screen.dart';
 import 'package:kurs_2021_app/screens/till_h_screen.dart';
 import 'package:kurs_2021_app/screens/uebung2_unbekannt_screen.dart';
 import 'package:kurs_2021_app/screens/unbekannt_screen.dart';
-
 
 
 class KursScreen extends StatelessWidget {
@@ -47,13 +39,7 @@ class KursScreen extends StatelessWidget {
             zeileKursteilnehmer("Jason Stolze", context, JasonSScreen()),
             zeileKursteilnehmer("Maximlian R", context, MaximilianRScreen()),
             zeileKursteilnehmer("Kerstin Geis", context, KerstinGScreen()),
-            zeileKursteilnehmer("Maximilian Desch", context, MaximilianDScreen()),
-            zeileKursteilnehmer("Sven Nowak", context, SvenNScreen()),
-            zeileKursteilnehmer("Keshia Kullmann", context, KeshiaKScreen()),
-            zeileKursteilnehmer("Katharina Schubert", context, KatharinaSScreen()),
-            zeileKursteilnehmer("Domenic Rosenbaum", context, DomenicRScreen()),
-            zeileKursteilnehmer("Carolin Steinborn", context, CarolinSScreen()),
-            zeileKursteilnehmer("Merle Lotz", context, MerleLScreen()),
+
           ]
         ),
       ),

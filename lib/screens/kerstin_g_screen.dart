893f9170e90addf9a634cbf984e3ab21b8741d99
KerstinGeis <kerstin.geis@informatik.hs-fulda.de>
2021-04-26 10:28:20 +0200

    Meine Seite

--- lib/screens/kerstin_g_screen.dart
+++ lib/screens/kerstin_g_screen.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class KerstinGScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromARGB(100, 224, 125, 238),
       appBar: AppBar(
         title: Text("Kerstin Geis"),
+        backgroundColor: Color.fromARGB(100, 224, 125, 238),
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
@@ -16,18 +19,30 @@ class KerstinGScreen extends StatelessWidget {
             SizedBox(
               height: 10,
             ),
-            Text(
-              "Name: Geis",
-            ),
-            Text(
-              "Vorname: Kerstin",
-            ),
-            Text(
-              "Fachbereich: Angewandte Informatik",
-            ),
-            Text(
-              "Studiengang: Digitale Medien",
-            )
+            Text("Name: Geis",
+                style: GoogleFonts.bangers(
+                    textStyle: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                ))),
+            Text("Vorname: Kerstin",
+                style: GoogleFonts.bangers(
+                    textStyle: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                ))),
+            Text("Fachbereich: Angewandte Informatik",
+                style: GoogleFonts.bangers(
+                    textStyle: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                ))),
+            Text("Studiengang: Digitale Medien",
+                style: GoogleFonts.bangers(
+                    textStyle: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                )))
           ],
         ),
       ),
